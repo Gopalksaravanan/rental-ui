e77a7a869f27f6a8765eb161871c822fa2aea608
Veera Sankar <sankarsivam6991@gmail.com>
2025-05-15 14:07:10 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className=" flex items-center justify-center h-screen">
+      <h1 className="font-bold text-5xl text-green-500">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
